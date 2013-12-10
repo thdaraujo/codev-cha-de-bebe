1acf8426b35901dc1f7c25c0411e4a8e80b91f4b
Thiago Araujo <thiago@carambola.com.vc>
2013-12-10 21:42:16 -0200

    Primeira versao do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChaDeBebe::Application.config.secret_key_base = '55357d5d3fe857f1210b55b657b99e347b8b5b473a4050db8002cfbba20cc8653fcde3931eddfccf7b5c9b0fdb37486b6e1b4a85ac4ad25bad1f21245d1246e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChaDeBebe::Application.config.session_store :cookie_store, key: '_cha-de-bebe_session'
