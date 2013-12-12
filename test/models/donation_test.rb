dc100ff053c3f9fb110a346af84f6a83375fcfe5
Thiago Araujo <thiago@carambola.com.vc>
2013-12-12 17:27:40 -0200

    testes para Donation

--- test/models/donation_test.rb
+++ test/models/donation_test.rb
@@ -6,29 +6,29 @@ class DonationTest < ActiveSupport::TestCase
 
 
    test "Create" do
-     assert_difference 'Donation.count' do
-	  Donation.create
-	 end
+    assert_difference 'Donation.count' do
+	    Donation.create
+	  end
    end
 
    test "Read" do
-     assert_equal [donations(:one), donations(:two)].sort, 
-       Donation.all.sort
-
+    assert_equal [donations(:one), donations(:two)].sort, 
+      Donation.all.sort
    end
 
    test "Update" do
-     #TODO
-     assert_equal [donations(:one), donations(:two)].sort, 
-       Donation.all.sort
+    assert_no_difference 'Donation.count' do
+     donations(:one).email = "novo@email.com"
+     assert donations(:one).save #retorna true ao salvar  
 
+     assert_equal "novo@email.com", donations(:one).email
+    end
    end
 
    test "Delete" do
-     #TODO
-     assert_equal [donations(:one), donations(:two)].sort, 
-       Donation.all.sort
-
+     assert_difference 'Donation.count', -1 do #diferenca de -1
+      donations(:one).destroy #delete
+     end
    end
 
 end
