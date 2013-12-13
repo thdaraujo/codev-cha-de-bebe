b1b4bd8345549a548e86631c400ec9859b5e5d24
Thiago Araujo <thiago@carambola.com.vc>
2013-12-13 17:40:57 -0200

    Controller para donations

--- test/controllers/donations_controller_test.rb
+++ test/controllers/donations_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class DonationsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "donations - rota /donations/id aponta para  com id" do
+  	#TODO precisa criar a doacao antes?
+  	assert_routing "/donations/123", {controller: "donations", action: "index", id: "123"}
+  end
+
+  test "donations - exibir doacao" do
+  	get :show
+  	#TODO parametros
+
+  	assert_response :success
+  end
+
+  test "donations - postar" do
+  	post :new
+  	#TODO campos
+
+
+  	assert_response :success
+  end
+
+  test "donations -  campos da doacao" do
+    get :show
+    #TODO
+    
+  end
+
 end
